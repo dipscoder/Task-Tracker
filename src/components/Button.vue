767cc02aec4b49991ecb0d12f4ab8374143ec83f
Dipesh Jaiswal <dipeshjaiswal140@gmail.com>
2021-05-18 13:55:11 +0530

    Add onClick event

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,5 +1,5 @@
 <template>
-    <button :style="{background: color}" class="btn">{{text}}</button>
+    <button @click="myClick()" :style="{background: color}" class="btn">{{text}}</button>
 </template>
 
 <script>
@@ -8,6 +8,11 @@ export default {
     props: {
         text: String,
         color: String,
-    }
+    },
+    methods: {
+        myClick() {
+            console.log("Clicked");
+        }
+    },
 }
 </script>
