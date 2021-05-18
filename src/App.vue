bbcc8227edeba4192a71cec329608de8f9e9b4fa
Dipesh Jaiswal <dipeshjaiswal140@gmail.com>
2021-05-18 15:56:16 +0530

    Emit event(toggle-reminder)

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="container">
     <Header title="Task Tracker" />
-    <Tasks @delete-task="deleteTask" :tasks="tasks" />
+    <Tasks
+      @toggle-reminder="toggleReminder"
+      @delete-task="deleteTask"
+      :tasks="tasks"
+    />
   </div>
 </template>
 
@@ -13,7 +17,7 @@ export default {
   name: "App",
   components: {
     Header,
-    Tasks, 
+    Tasks,
   },
 
   data() {
@@ -24,10 +28,19 @@ export default {
   methods: {
     deleteTask(id) {
       // console.log(id);
-      if (confirm('Are you sure?')) {
-        this.tasks = this.tasks.filter((task) => {return task.id !== id})
+      if (confirm("Are you sure?")) {
+        this.tasks = this.tasks.filter((task) => {
+          return task.id !== id;
+        });
       }
-    }
+    },
+
+    toggleReminder(id) {
+      // console.log(id);
+      this.tasks = this.tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      );
+    },
   },
   created() {
     this.tasks = [
